4e54c31861c4bcbeff1e7277f50ddc140b5f90f3
amans102 <108821814+amans102@users.noreply.github.com>
2023-08-26 12:47:45 +0000

    Container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "ajwdnwjd2@8u3gmail.com"
+var accountPassword = "183793"
+accountCity = "Jaipur"
+
+//accountId = 2  //not allowed
+
+console.log(accountId);
+/* 
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+ "use strict";
+
+ //alert( 3 + 3)//we are using nodejs not broweser
+ console.log(3+30)
+
+ console.log("aman")
+
+ let name = "aman"
+ let age = 23
+ let isLogged = false
+
+
+ //number => 2 to the power 53
+ //bigint
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+ // undefined =>
+ // symbol => unique
+
+
+
+ //object
+
+ console.log(typeof undefined)
+ console.log(typeof null)
